788e0da350c82b9bd06927ff6165375171c58c1d
Hafidz Muzakky <ais.muzakky@gmail.com>
2022-12-17 20:11:14 +0700

    joyeuse: Bringing up FBEv1 decryption
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -8,6 +8,16 @@
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Encryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    fbe.metadata.wrappedkey=true
+
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
