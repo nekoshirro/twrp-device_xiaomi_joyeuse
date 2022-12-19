91240ade2044d0aa449bfb953ade6519e34a769d
Hafidz Muzakky <ais.muzakky@gmail.com>
2022-12-19 23:10:30 +0700

    [TEMP] joyeuse: Stop cloning kernel source by vendorsetup

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-git clone -q https://github.com/nekoshirro/platform_kernel_xiaomi_sm6250.git --depth 1 kernel/xiaomi/joyeuse
